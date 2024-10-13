377168d222304796b57abb875b0ec2f6166bb4d1
duyan0 <crandi21112004@gmail.com>
2024-10-13 19:11:54 +0700

    add project code

---
+++ BanSach/BanSach/Controllers/DanhMucsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BanSach.Models;
+
+namespace BanSach.Controllers
+{
+    public class DanhMucsController : Controller
+    {
+        private SachEntities db = new SachEntities();
+
+        // Action PartialViewResult
+        [AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]
+        public PartialViewResult PhanDanhMuc()
+        {
+            var cateList = db.DanhMuc.ToList();
+            return PartialView(cateList);
+        }
+
+        // GET: DanhMucs
+        public ActionResult Index()
+        {
+            return View(db.DanhMuc.ToList());
+        }
+
+        // GET: DanhMucs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DanhMuc danhMuc = db.DanhMuc.Find(id);
+            if (danhMuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(danhMuc);
+        }
+
+        // GET: DanhMucs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DanhMucs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,DanhMuc1,TheLoai")] DanhMuc danhMuc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DanhMuc.Add(danhMuc);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(danhMuc);
+        }
+
+        // GET: DanhMucs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DanhMuc danhMuc = db.DanhMuc.Find(id);
+            if (danhMuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(danhMuc);
+        }
+
+        // POST: DanhMuc/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,DanhMuc1,TheLoai")] DanhMuc danhMuc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(danhMuc).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(danhMuc);
+        }
+
+        // GET: DanhMuc/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DanhMuc danhMuc = db.DanhMuc.Find(id);
+            if (danhMuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(danhMuc);
+        }
+
+        // POST: DanhMuc/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DanhMuc danhMuc = db.DanhMuc.Find(id);
+            db.DanhMuc.Remove(danhMuc);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
