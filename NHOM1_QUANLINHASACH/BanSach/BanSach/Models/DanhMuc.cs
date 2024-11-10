201ce6ca37482078e75288d84e9c1b24e143eaa1
duyan0 <147116622+duyan0@users.noreply.github.com>
2024-11-10 09:46:19 +0700

    V3.3.1 - Fix 1 số chức năng

--- NHOM1_QUANLINHASACH/BanSach/BanSach/Models/DanhMuc.cs
+++ NHOM1_QUANLINHASACH/BanSach/BanSach/Models/DanhMuc.cs
@@ -21,6 +21,7 @@ namespace BanSach.Models
 
         [Required(ErrorMessage = "Thể loại không được để trống")]
         [StringLength(50, ErrorMessage = "Thể loại không được vượt quá 50 ký tự")]
+        
         public string TheLoai { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
